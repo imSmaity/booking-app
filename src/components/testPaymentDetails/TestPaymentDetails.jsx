f3659e58af100d77d5e0f1c5432269245d45e2c0
Suman <60940736+imSmaity@users.noreply.github.com>
2022-03-25 17:23:22 +0530

    update

--- src/components/testPaymentDetails/TestPaymentDetails.jsx
+++ src/components/testPaymentDetails/TestPaymentDetails.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import warning from '../../Assets/warning.png'
 
+
+const paymentDetails={
+  email:'sb-lhqpk11017525@business.example.com',
+  password:'&wK7!fn+'
+}
+
 export default function TestPaymentDetails() {
     const [detail,setDetails]=useState(false)
 
@@ -8,8 +14,8 @@ export default function TestPaymentDetails() {
     <div className='p-3'>
         <img src={warning} alt='warning' onMouseOver={()=>setDetails(true)} onMouseOut={()=>setDetails(false)}/>
         <span className='ms-2' style={{fontSize:'small'}}>
-        <strong>Email:</strong> sb-osl43i11018844@personal.example.com
-        <div className='ms-4'><strong>Password:</strong>{` ${'"z${Svo6'}`}</div></span>
+        <strong>Email: </strong> {paymentDetails.email}
+        <div className='ms-4'><strong>Password: </strong>{paymentDetails.password}</div></span>
         {detail && <div style={{position:"absolute",fontSize:'small',background:"white"}} className='p-1'>Test payment details</div>}
     </div>
   )
